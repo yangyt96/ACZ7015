a39c87771c444a1067e29de1749f27007a8fb323
yangyt96 <yeeyang.tan@live.com>
2024-08-27 20:18:01 +0200

    [chore] add compile proj script

--- project.tcl
+++ project.tcl
@@ -1,3 +1,10 @@
+set project_name "project_test"
+set project_dir "C:/project/ACZ7015/workspace/$project_name"
+
+
+create_project $project_name $project_dir -part xc7z015clg485-2
+
+
 
 
 set cur_dir [file dirname [info script]]
@@ -5,7 +12,16 @@ set cur_dir [file dirname [info script]]
 
 set lib_file "$cur_dir/lib/source_lib.tcl"
 set bd_file "$cur_dir/bd/board_diagram.tcl"
+set xdc_file "$cur_dir/xdc/DDR_LCD.xdc"
 
 
+# add library
 source $lib_file
+
+# add board diagram
 source $bd_file
+
+
+# add constraint
+add_files $xdc_file
+
